c3b364826f7d82072f4fb46e2d4d64f8d59c45a1
kahaaga <kahaaga@gmail.com>
2018-01-15 18:20:14 +0100

    Added tests.

--- test/random_centroids.jl
+++ test/random_centroids.jl
@@ -1,4 +1,6 @@
 using Distributions
+using InvariantDistribution
+using SimplexSplitting
 using TransferEntropy
 using PlotlyJS
 #addprocs(2)
@@ -22,39 +24,101 @@ end
 
 using BenchmarkTools
 
-@time jointtest()
+#@time jointtest()
 
-@time joint =
-Py, Pxy, Pxz, = TransferEntropy.marginaldists(unique(bins[find(measure .> 0), :], 1), measure)
-@test size(joint) == size(Py) == size(Pxy) == size(Pxz)
+#Py, Pxy, Pxz, = TransferEntropy.marginaldists(unique(bins[find(measure .> 0), :], 1), measure)
+#@test size(joint) == size(Py) == size(Pxy) == size(Pxz)
 
-dist = Normal()
+#dist = Normal()
 
-function te_over_binsizes(binsizes; npts::Int = 1000)
+function te_over_binsizes(binsizes; npts::Int = 1000, covariance = 0.4)
+    # Create an uncorrelated source and target
     dist = Normal()
-    centroids = rand(dist, npts, 3)
-    invmeasure = abs.(rand(dist, npts))
-    invmeasure[randperm(400)[50]] = 0
-    invmeasure = invmeasure ./ sum(invmeasure) # normalise to true probability dist.
+
+    source = rand(dist, npts, 1)
+    dest = covariance .* source[1:end] .+ (1.0 - covariance) .* rand(dist, npts, 1)
+
+    embedding = hcat(source[1:end-1], source[2:end], dest[2:end])
+
+    if invariantset(embedding)
+      #println("The embedding forms an invariant set. Continuing.")
+    else
+      #println("The embedding does not form an invariant set. Quitting.")
+      return zeros(length(binsizes))
+    end
+
+    # Embed using all points except the last (to allow projection of all vertices
+    # in the triangulation).
+    points, simplex_inds = triangulate(embedding[1:end-1, :])
+    image_points = embedding[2:end, :]
+
+    P = markovmatrix(points, image_points, simplex_inds)
+
+    invmeasure, inds_nonzero_simplices = invariantdist(P)
+    #centroids = rand(dist, npts, 3)
+    #invmeasure = abs.(rand(dist, npts))
+    #invmeasure[randperm(400)[50]] = 0
+    #invmeasure = invmeasure ./ sum(invmeasure) # normalise to true probability dist.
 
     TE = zeros(length(binsizes))
 
     count = 0
     for binsize in binsizes
         count +=1
-        TE[count] = te_from_triangulation(centroids, invmeasure, binsize)
+        te = te_from_triangulation(embedding, invmeasure, binsize)
+        TE[count] = te
     end
-
     return TE
 end
 
-using Base.Threads
-@show nprocs()
+##################################################################
+# Calculate TE for correlated gaussians over a range of bin sizes
+##################################################################
+binsizes = 1:1:100
+covars = 0:0.1:0.9
+reps = 500
+npts = 1000
+TEs = zeros(Float64, length(binsizes), reps, length(covars))
 
-binsizes = 1:5
-TE = te_over_binsizes(binsizes)
-plot(scatter(;x = binsizes, y = TE))
+for j = 1:length(covars)
+    covar = covars[j]
+    println("Covariance: ", covar, " Expected TE: ", log(1/(1 - covar^2)))
+    for i = 1:reps
+        println("\tRep #", i)
+        te = te_over_binsizes(binsizes; npts = npts, covariance = covar)
+        TEs[1:end, i, j] = te
+    end
 
+    #@show size(TEs[1:end, 1:end, j])
+    #@show TEs[1:end, 1:end, j]
+    #@show mean(TEs[length(binsizes)-5:end, 1:end, j], 1)
+    #println("")
+    #mean_this_covar = mean(TEs[1:end, 1:end, j])
+
+    #println(" Expected TE: ", log(1/(1 - covar^2)), " ,", log(1/(1 - covar^2)/log(2)), " Computed TE: ", mean_this_covar)
+end
+####################
+# PLOT THE TE CURVES
+####################
+data = PlotlyJS.GenericTrace[]
+layout = PlotlyJS.Layout(autosize = true,
+                          margin = PlotlyJS.attr(l=0, r=0, b=0, t=65),
+                          xaxis = PlotlyJS.attr(title = "Bin size", ticks = "outside"),
+                          yaxis = PlotlyJS.attr(title = "TE (nats)"))
+for i = 1:reps
+    trace = PlotlyJS.scatter(;x = binsizes,
+                               y = TEs[1:end, i],
+                               mode = "markers+lines",
+                               showlegend = false,
+                               marker = PlotlyJS.attr(size = 3, color = "black"),
+                               line = PlotlyJS.attr(size = 0.8, color = "white"))
+    push!(data, trace)
+end
+#push!(data, PlotlyJS.scatter(;x = binsizes, y = TEs, mode = "lines"))
+push!(data, PlotlyJS.scatter(; x = binsizes, y = fill(log(1/(1-covar^2)), length(binsizes)) ))
+PlotlyJS.plot(data, layout)
+
+#plot(scatter(;x = binsizes, y = te_over_binsizes(binsizes; covariance = covar)))
 
 #@show te
 #binsizes = 1:100
