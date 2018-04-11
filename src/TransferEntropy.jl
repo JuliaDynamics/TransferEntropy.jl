cf9f57b2918a1795e8ca874468c858e8d948a41a
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-04-11 12:02:11 +0200

    Include examples.

--- src/TransferEntropy.jl
+++ src/TransferEntropy.jl
@@ -1,14 +1,10 @@
 __precompile__(true)
+
 module TransferEntropy
 
-using Distributions
-using Simplices
-using SimplexSplitting
-using InvariantDistribution
-using TransferEntropy
-using ChaoticMaps
-using ProgressMeter
-using PmapProgressMeter
+using Distributions, Simplices, SimplexSplitting, InvariantDistribution, TransferEntropy,
+	ChaoticMaps, ProgressMeter, PmapProgressMeter
+
 @everywhere using Distributions
 @everywhere using Simplices
 @everywhere using SimplexSplitting
@@ -27,11 +23,14 @@ include("TEresult.jl")
 include("te_from_embedding.jl")
 include("te_from_triang.jl")
 include("te_from_ts.jl")
+include("TE_examples.jl")
 
-export indexin_rows, get_nonempty_bins, get_nonempty_bins_abs,
+export indexin_rows,
+	get_nonempty_bins, get_nonempty_bins_abs,
         jointdist, marginaldists,
         te_from_embedding,
         te_from_triang,
         te_from_ts,
-        TEresult
+        TEresult,
+        Examples
 end # module
