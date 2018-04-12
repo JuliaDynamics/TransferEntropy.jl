ec326253bdc88ae0e1b3443981ff821182ff2b8f
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-04-12 14:14:01 +0200

    Find row indices for arrays containing floats

--- src/rowindexin.jl
+++ src/rowindexin.jl
@@ -12,3 +12,15 @@ function indexin_rows(A1::Array{Int, 2}, A2::Array{Int, 2})
     end
     return inds
 end
+
+function indexin_rows(A1::Array{Float64, 2}, A2::Array{Float64, 2})
+    inds = []
+    for j = 1:size(A1, 1)
+        for i = 1:size(A2, 1)
+            if all(A1[j, :] .== A2[i, :])
+                push!(inds, i)
+            end
+        end
+    end
+    return inds
+end
