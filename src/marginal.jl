b1e199df2e870ba5d8e0297e95b3ea61fda7a31f
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-08-20 18:38:32 +0200

    Separate out marginal funcs

--- src/marginal.jl
+++ src/marginal.jl
@@ -1,70 +1,51 @@
-using GroupSlices
+"""
+Returns a column vector with the same number of elements as there are unique
+rows in A. The value of the ith element is the row indices of rows in A
+matching the ith unique row.
+"""
+marginal_indices(A) = GroupSlices.groupinds(GroupSlices.groupslices(A, 1))
 
 """
-    marginaldists(nonempty_bins::Array{Int, 2},
-                  invmeasure::Vector{Float64})
-
-Compute marginal distributions for a triangulation with an associated invariant
-measure (`invmeasure`).
-
-Computations are performed as follows:
-
-- Find indices of non-empty bins beforehand (tuples, where each component gives
-the index along each axis of the state space). This is information is calculated
-beforehand, and is provided with the `nonempty_bins::Array{Int, 2}` argument.
-- Compute the marginal distributions using the invariant measures associated
-with the nonempty bins.
+How many times does each unique row in A appear? Returns a column vector with the same
+number of elements as there are unique rows in A. The value of the ith element of the
+return vector is the number of times the ith unique row of A appears in A.
+"""
+marginal_multiplicity(A) = [length(x) for x in marginal_indices(A)]
 
 """
-function marginaldists(nonempty_bins::Array{Int, 2},
-                       invdist::Array{Float64, 1})
+Compute the marginal for a binning with an associated transfer operator. The
+    marginal is computed for the columns `cols`.
+"""
+function marginal(cols::Vector{Int},
+                    rb::StateSpaceReconstruction.RectangularBinning,
+                    to::PerronFrobenius.RectangularBinningTransferOperator,
+                    iv::PerronFrobenius.InvariantDistribution)
+    positive_measure_bins = rb.unique_nonempty_bins[iv.nonzero_inds, :]
 
-    #unique_nonempty_bins = unique(nonempty_bins, 1)
+    # Loop over the positively measured bins.
+    marginal_inds::Vector{Vector{Int}} = marginal_indices(positive_measure_bins[:, cols])
+    marginal = zeros(Float64, size(marginal_inds, 1))
 
-    dim = size(nonempty_bins, 2)
-
-
-    X2s = nonempty_bins[:, 2] # Vector{Float64}
-    X2s = reshape(X2s, length(X2s), 1) # Reshape to Array{Float64, 2}
-    unique_X2s = unique(X2s, 1)
-    J_X2 = indexin_rows(X2s, unique_X2s)
-
-
-    marginal_x2 = zeros(Float64, size(unique_X2s, 1))
-
-    for i = 1:size(unique_X2s, 1)
-        multiplicity_i = find(J_X2 .== i)
-        marginal_x2[i] = sum(invdist[multiplicity_i])
+    for i = 1:size(marginal_inds, 1)
+        marginal[i] = sum(iv.dist[iv.nonzero_inds][marginal_inds[i]])
     end
 
-
-    # X1 and X2
-    X1X2s = nonempty_bins[:, 1:2]
-    unique_X1X2s = unique(X1X2s, 1)
-    J_X1X2 = indexin_rows(X1X2s, unique_X1X2s)
-
-    marginal_x1x2 = zeros(Float64, size(unique_X1X2s, 1))
-    for i = 1:size(unique_X1X2s, 1)
-        multiplicity_i = J_X1X2 .== i
-        marginal_x1x2[i] = sum(invdist[multiplicity_i])
-    end
-
-    # X2 and X3
-    X2X3s = nonempty_bins[:, 2:3]
-    unique_X2X3s = unique(X2X3s, 1)
-    J_X2X3 = indexin_rows(X2X3s, unique_X2X3s)
-
-    marginal_x2x3 = zeros(Float64, size(unique_X2X3s, 1))
-    for i = 1:size(unique_X2X3s, 1)
-        multiplicity_i = J_X2X3 .== i
-        marginal_x2x3[i] = sum(invdist[multiplicity_i])
-    end
-
-    @assert sum(marginal_x2) ≈ 1
-    @assert sum(marginal_x1x2) ≈ 1
-    @assert sum(marginal_x2x3) ≈ 1
-
-    return marginal_x2, marginal_x1x2, marginal_x2x3,
-            J_X2, J_X1X2, J_X2X3
-
+    return marginal
 end
+
+function marginal(cols::Vector{Int},
+                    positive_measure_bins::Array{Int, 2},
+                    iv::PerronFrobenius.InvariantDistribution)
+
+    marginal_inds::Vector{Vector{Int}} = marginal_indices(positive_measure_bins[:, cols])
+
+    # Only loop over the nonzero entries of the invariant distribution
+    nonzero_elements_of_dist = iv.dist[iv.nonzero_inds]
+    marginal = zeros(Float64, size(marginal_inds, 1))
+    @inbounds for i = 1:size(marginal_inds, 1)
+        marginal[i] = sum(nonzero_elements_of_dist[marginal_inds[i]])
+    end
+    return marginal
+end
+
+export marginal_indices, marginal_multiplicity, marginal
