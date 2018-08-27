caf68be4a9909c215ed6af6e659d80bd0c128a66
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-08-27 12:30:22 +0200

    Removed old tests.

--- test/te_from_ts.jl
+++
@@ -1,9 +0,0 @@
-
-dt = float.(readcsv("/Users/kristian/Dropbox/PhD/Fo3_tmp.txt")[2:end, :])
-
-println("TE SETUP")
-te = te_from_ts(dt[1:50, 1], dt[1:50, 2], binsizes = [20, 30])
-te = te_from_ts(dt[1:50, 1], dt[1:50, 2], binsizes = collect(1:50))
-
-println("TE(Fo3 -> Temp)")
-te1_to_2_new = te_from_ts(dt[:, 1], dt[:, 2], n_reps = 1, binsizes = [100])
