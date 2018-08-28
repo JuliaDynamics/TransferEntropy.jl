84045b9638e4f1c0d301e0270545bfafbcb86d8b
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-08-28 12:35:58 +0200

    Remove old exports and tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,24 +3,3 @@ using Base.Test
 
 # write your own tests here
 #include("random_centroids.jl")
-
-
-
-@testset "Transfer entropy" begin
-
-	x = rand(50)
-	y = rand(50)
-	embedding1 = embed_for_te(x, y, 1)
-	embedding2 = embed_for_te(x, y, -1)
-
-	@test !isempty(embedding1)
-	@test !isempty(embedding2)
-
-	te = te_from_ts(x, y, n_reps = 1)
-	@test length(te) == 5
-
-
-end
-
-
-include("te_from_embedding.jl")
