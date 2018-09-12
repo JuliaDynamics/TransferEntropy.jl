e820277cd0295b76650af8186143816e952d2bfd
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-09-12 14:29:16 +0200

    Allow different number of nearest neighbors
    The lowest-dimensional mutual information estimate needs a higher number of nearest neighbours to be accurate. The number of nearest neighbours for the highest-dimensional MI is now given as `k1`, and the corresponding number for the lowest-dimensional MI is given as `k2`.

--- src/estimators/transferentropy_kraskov.jl
+++ src/estimators/transferentropy_kraskov.jl
@@ -12,15 +12,25 @@ function marginal_NN(points, dists_to_kth)
 end
 
 """
-    transferentropy_kraskov(points::AbstractArray{T, 2}, k::Int, v::TEVars)
+    transferentropy_kraskov(points::AbstractArray{T, 2}, k1::Int, k2::Int,
+        v::TEVars; metric = Chebyshev())
 
-Compute transfer entropy using an adapted version of the Kraskov estimator for
-mutual information [1].
+Compute transfer entropy decomposed as the sum of mutual informations,
+using an adapted version of the Kraskov estimator for mutual information [1].
 
 ## Arguments
 - `points`: The set of points representing the embedding for which to compute
     transfer entropy. Must be provided as an array of size `dim`-by-`npoints.
-- `k`: The number of nearest neighbours
+- `k1`: The number of nearest neighbours for the highest-dimensional mutual
+    information estimate. To minimize bias, choose ``k_1 < k_2`` if
+    ``min(k_1, k_2) < 10`` (see fig. 16 in [1]). Beyond dimension 5, choosing
+    ``k_1 = k_2`` results in fairly low bias, and a low number of nearest
+    neighbours, say `k1 = k2 = 4`, will suffice.
+- `k2`: The number of nearest neighbours for the lowest-dimensional mutual
+    information estimate. To minimize bias, choose ``k_1 < k_2`` if
+    if ``min(k_1, k_2) < 10`` (see fig. 16 in [1]). Beyond dimension 5, choosing
+    ``k_1 = k_2`` results in fairly low bias, and a low number of nearest
+    neighbours, say `k1 = k2 = 4`, will suffice.
 - `v`: A `TEVars` instance, indicating which variables of the embedding should
     be grouped as what when computing the marginal entropies that go into the
     transfer entropy expression.
@@ -32,8 +42,8 @@ mutual information [1].
 1. Kraskov, Alexander, Harald Stögbauer, and Peter Grassberger. "Estimating
     mutual information." Physical review E 69.6 (2004): 066138.
 """
-function transferentropy_kraskov(points::AbstractArray{T, 2}, k::Int, v::TEVars;
-            metric = Chebyshev()) where T
+function transferentropy_kraskov(points::AbstractArray{T, 2}, k1::Int, k2::Int,
+        v::TEVars; metric = Chebyshev()) where T
 
     # Make sure that the array contains points as columns.
     if size(points, 1) > size(points, 2)
@@ -61,13 +71,13 @@ function transferentropy_kraskov(points::AbstractArray{T, 2}, k::Int, v::TEVars;
     tree_XY = KDTree(pts_XY, metric)
 
     # Find the k nearest neighbors to all of the points in each of the trees
-    idxs_XYZ, dists_XYZ = knn(tree_XYZ, pts_XYZ, k, true)
-    idxs_XY, dists_XY   = knn(tree_XY,  pts_XY, k, true)
+    idxs_XYZ, dists_XYZ = knn(tree_XYZ, pts_XYZ, k1, true)
+    idxs_XY, dists_XY   = knn(tree_XY,  pts_XY, k2, true)
 
     # In each of the trees, find the index of the k-th nearest neighbor to all
     # of the points.
-    kth_NN_idx_XYZ = [idx[k] for idx in idxs_XYZ]
-    kth_NN_idx_XY  = [idx[k] for idx in idxs_XY]
+    kth_NN_idx_XYZ = [idx[k1] for idx in idxs_XYZ]
+    kth_NN_idx_XY  = [idx[k2] for idx in idxs_XY]
 
     # Distances between points in the XYZ and XY spaces and their
     # kth nearest neighbour, along marginals X, YZ (for the XYZ space)
@@ -89,20 +99,29 @@ end
 
 
 """
-    transferentropy_kraskov(points::AbstractArray{T, 2}, k::Int,
+    transferentropy_kraskov(points::AbstractArray{T, 2}, k1::Int, k2::Int,
         target_future::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         target_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         source_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         conditioned_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}};
         metric = Chebyshev()) where T
 
-Compute transfer entropy using an adapted version of the Kraskov estimator for
-mutual information [1].
+Compute transfer entropy decomposed as the sum of mutual informations,
+using an adapted version of the Kraskov estimator for mutual information [1].
 
 ## Arguments
 - `points`: The set of points representing the embedding for which to compute
     transfer entropy. Must be provided as an array of size `dim`-by-`npoints.
-- `k`: The number of nearest neighbours
+- `k1`: The number of nearest neighbours for the highest-dimensional mutual
+    information estimate. To minimize bias, choose ``k_1 < k_2`` if
+    ``min(k_1, k_2) < 10`` (see fig. 16 in [1]). Beyond dimension 5, choosing
+    ``k_1 = k_2`` results in fairly low bias, and a low number of nearest
+    neighbours, say `k1 = k2 = 4`, will suffice.
+- `k2`: The number of nearest neighbours for the lowest-dimensional mutual
+    information estimate. To minimize bias, choose ``k_1 < k_2`` if
+    if ``min(k_1, k_2) < 10`` (see fig. 16 in [1]). Beyond dimension 5, choosing
+    ``k_1 = k_2`` results in fairly low bias, and a low number of nearest
+    neighbours, say `k1 = k2 = 4`, will suffice.
 - `v`: A `TEVars` instance, indicating which variables of the embedding should
     be grouped as what when computing the marginal entropies that go into the
     transfer entropy expression.
@@ -123,7 +142,7 @@ mutual information [1].
 1. Kraskov, Alexander, Harald Stögbauer, and Peter Grassberger. "Estimating
     mutual information." Physical review E 69.6 (2004): 066138.
 """
-function transferentropy_kraskov(points::AbstractArray{T, 2}, k::Int,
+function transferentropy_kraskov(points::AbstractArray{T, 2}, k1::Int, k2::Int,
         target_future::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         target_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         source_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
@@ -139,24 +158,33 @@ function transferentropy_kraskov(points::AbstractArray{T, 2}, k::Int,
     v = TEVars(target_future, target_presentpast,
                 source_presentpast, conditioned_presentpast)
 
-    transferentropy_kraskov(points, k, v)
+    transferentropy_kraskov(points, k1, k2, v)
 end
 
 
 """
-    transferentropy_kraskov(points::AbstractArray{T, 2}, k::Int,
+    transferentropy_kraskov(points::AbstractArray{T, 2}, k1::Int, k2::Int,
         target_future::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         target_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         source_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}};
         metric = Chebyshev()) where T
 
-Compute transfer entropy using an adapted version of the Kraskov estimator for
-mutual information [1].
+Compute transfer entropy decomposed as the sum of mutual informations,
+using an adapted version of the Kraskov estimator for mutual information [1].
 
 ## Arguments
 - `points`: The set of points representing the embedding for which to compute
     transfer entropy. Must be provided as an array of size `dim`-by-`npoints.
-- `k`: The number of nearest neighbours
+- `k1`: The number of nearest neighbours for the highest-dimensional mutual
+    information estimate. To minimize bias, choose ``k_1 < k_2`` if
+    ``min(k_1, k_2) < 10`` (see fig. 16 in [1]). Beyond dimension 5, choosing
+    ``k_1 = k_2`` results in fairly low bias, and a low number of nearest
+    neighbours, say `k1 = k2 = 4`, will suffice.
+- `k2`: The number of nearest neighbours for the lowest-dimensional mutual
+    information estimate. To minimize bias, choose ``k_1 < k_2`` if
+    if ``min(k_1, k_2) < 10`` (see fig. 16 in [1]). Beyond dimension 5, choosing
+    ``k_1 = k_2`` results in fairly low bias, and a low number of nearest
+    neighbours, say `k1 = k2 = 4`, will suffice.
 - `v`: A `TEVars` instance, indicating which variables of the embedding should
     be grouped as what when computing the marginal entropies that go into the
     transfer entropy expression.
@@ -177,7 +205,7 @@ This version of the function assumes there is no conditioning.
 1. Kraskov, Alexander, Harald Stögbauer, and Peter Grassberger. "Estimating
     mutual information." Physical review E 69.6 (2004): 066138.
 """
-function transferentropy_kraskov(points::AbstractArray{T, 2}, k::Int,
+function transferentropy_kraskov(points::AbstractArray{T, 2}, k1::Int, k2::Int,
         target_future::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         target_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         source_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}};
@@ -192,19 +220,28 @@ function transferentropy_kraskov(points::AbstractArray{T, 2}, k::Int,
     v = TEVars(target_future, target_presentpast,
                 source_presentpast, Int[])
 
-    transferentropy_kraskov(points, k, v)
+    transferentropy_kraskov(points, k1, k2, v)
 end
 
 """
     transferentropy_kraskov(E::StateSpaceReconstruction.AbstractEmbedding,
-            k::Int, v::TEVars; metric = Chebyshev())
+            k1::Int, k2::Int, v::TEVars; metric = Chebyshev())
 
-Compute transfer entropy using an adapted version of the Kraskov estimator for
-mutual information [1].
+Compute transfer entropy decomposed as the sum of mutual informations,
+using an adapted version of the Kraskov estimator for mutual information [1].
 
 Arguments:
 - `E`: The embedding for which to compute transfer entropy.
-- `k`: The number of nearest neighbours
+- `k1`: The number of nearest neighbours for the highest-dimensional mutual
+    information estimate. To minimize bias, choose ``k_1 < k_2`` if
+    ``min(k_1, k_2) < 10`` (see fig. 16 in [1]). Beyond dimension 5, choosing
+    ``k_1 = k_2`` results in fairly low bias, and a low number of nearest
+    neighbours, say `k1 = k2 = 4`, will suffice.
+- `k2`: The number of nearest neighbours for the lowest-dimensional mutual
+    information estimate. To minimize bias, choose ``k_1 < k_2`` if
+    if ``min(k_1, k_2) < 10`` (see fig. 16 in [1]). Beyond dimension 5, choosing
+    ``k_1 = k_2`` results in fairly low bias, and a low number of nearest
+    neighbours, say `k1 = k2 = 4`, will suffice.
 - `v`: A `TEVars` instance, indicating which variables of the embedding should
     be grouped as what when computing the marginal entropies that go into the
     transfer entropy expression.
@@ -214,31 +251,40 @@ Arguments:
 mutual information." Physical review E 69.6 (2004): 066138.
 """
 function transferentropy_kraskov(E::StateSpaceReconstruction.AbstractEmbedding{D, T},
-            k::Int, v::TEVars; metric = Chebyshev()) where {D, T}
+            k1::Int, k2::Int, v::TEVars; metric = Chebyshev()) where {D, T}
 
     # Make sure that the array contains points as columns.
     if size(E.points, 1) > size(E.points, 2)
         error("The dimension exceeds the number of points.")
     end
 
-    transferentropy_kraskov(E.points, k, v, metric = metric)
+    transferentropy_kraskov(E.points, k1, k2, v, metric = metric)
 end
 
 """
-    transferentropy_kraskov(E::AbstractEmbedding, k::Int,
+    transferentropy_kraskov(E::AbstractEmbedding, k1::Int, k2::Int,
         target_future::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         target_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         source_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         conditioned_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}};
         metric = Chebyshev()) where T
 
-Compute transfer entropy using an adapted version of the Kraskov estimator for
-mutual information [1].
+Compute transfer entropy decomposed as the sum of mutual informations,
+using an adapted version of the Kraskov estimator for mutual information [1].
 
 ## Arguments
 - `points`: The set of points representing the embedding for which to compute
     transfer entropy. Must be provided as an array of size `dim`-by-`npoints.
-- `k`: The number of nearest neighbours
+- `k1`: The number of nearest neighbours for the highest-dimensional mutual
+    information estimate. To minimize bias, choose ``k_1 < k_2`` if
+    ``min(k_1, k_2) < 10`` (see fig. 16 in [1]). Beyond dimension 5, choosing
+    ``k_1 = k_2`` results in fairly low bias, and a low number of nearest
+    neighbours, say `k1 = k2 = 4`, will suffice.
+- `k2`: The number of nearest neighbours for the lowest-dimensional mutual
+    information estimate. To minimize bias, choose ``k_1 < k_2`` if
+    if ``min(k_1, k_2) < 10`` (see fig. 16 in [1]). Beyond dimension 5, choosing
+    ``k_1 = k_2`` results in fairly low bias, and a low number of nearest
+    neighbours, say `k1 = k2 = 4`, will suffice.
 - `v`: A `TEVars` instance, indicating which variables of the embedding should
     be grouped as what when computing the marginal entropies that go into the
     transfer entropy expression.
@@ -258,7 +304,7 @@ mutual information [1].
 1. Kraskov, Alexander, Harald Stögbauer, and Peter Grassberger. "Estimating
     mutual information." Physical review E 69.6 (2004): 066138.
 """
-function transferentropy_kraskov(E::AbstractEmbedding{D, T}, k::Int,
+function transferentropy_kraskov(E::AbstractEmbedding{D, T}, k1::Int, k2::Int,
         target_future::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         target_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
         source_presentpast::Union{Int, UnitRange{Int}, Vector{Int}, Tuple{Int}},
@@ -274,7 +320,7 @@ function transferentropy_kraskov(E::AbstractEmbedding{D, T}, k::Int,
     v = TEVars(target_future, target_presentpast,
                 source_presentpast, conditioned_presentpast)
 
-    transferentropy_kraskov(E.points, k, v)
+    transferentropy_kraskov(E.points, k1, k2, v)
 end
 
 tekraskov = transferentropy_kraskov
