6992131a21d8fe2c4d56fc5ee0629898624645bd
Kristian Agasøster Haaga <kahaaga@gmail.com>
2020-12-18 14:27:29 +0100

    Demand latest version for Entropies.jl

--- src/transferentropy/binning_based.jl
+++ src/transferentropy/binning_based.jl
@@ -1,4 +1,6 @@
-import Entropies: VisitationFrequency, RectangularBinning
-export VisitationFrequency, RectangularBinning
+# Binning schemes
+import Entropies: RectangularBinning
+export RectangularBinning
 
+include("visitationfrequency.jl")
 include("transferoperator.jl")
